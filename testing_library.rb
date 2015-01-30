95ff527baa8e0c243f07825345ad0991bff80044
Andrew Deitrick <ajdeitrick@gmail.com>
2015-01-29 20:32:54 -0700

    update for handling multiple exercises

--- testing_library.rb
+++ testing_library.rb
@@ -24,18 +24,18 @@ def check(method_to_check, returned_value, expected_value)
 end
 
 def send_results(results)
-  conn = Faraday.new(:url => 'https://secret-shelf-7893.herokuapp.com/') do |faraday|
+  conn = Faraday.new(:url => 'http://localhost:3000') do |faraday|
     faraday.request  :url_encoded
-    faraday.adapter  Faraday.default_adapter 
+    faraday.adapter  Faraday.default_adapter
   end
 
   counts = Hash.new 0
 
-  results.each do |word|
-    counts[word] += 1
+  results.each do |result|
+    counts[result] += 1
   end
 
   name = File.open(".name").read
 
-  conn.post '/submissions', { :user => name, passed: counts["passed"], failed: counts["failed"] }
+  conn.post '/submissions', { :user => name, passed: counts["passed"], failed: counts["failed"], exercise: "array-warmup-easy" }
 end
